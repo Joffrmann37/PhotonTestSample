0512c8245e56071d352b12d946696aaa9983d5e8
Joffrey Mann <joffrey@jfammobility.com>
2024-02-07 11:01:53 -0500

    Initial Commit

---
+++ NYCHighSchools/NYCHighSchoolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCHighSchoolsApp.swift
+//  NYCHighSchools
+//
+//  Created by Joffrey Mann on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NYCHighSchoolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
