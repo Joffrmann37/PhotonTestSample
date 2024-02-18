5321b111e0446028c9ca233bae712e9c248565d1
Joffrey Mann <joffrey@jfammobility.com>
2024-02-18 00:20:45 -0500

    Added framework to folder

---
+++ CleanArchitecture.framework/Headers/CleanArchitecture.h
@@ -0,0 +1,18 @@
+//
+//  CleanArchitecture.h
+//  CleanArchitecture
+//
+//  Created by Joffrey Mann on 2/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CleanArchitecture.
+FOUNDATION_EXPORT double CleanArchitectureVersionNumber;
+
+//! Project version string for CleanArchitecture.
+FOUNDATION_EXPORT const unsigned char CleanArchitectureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CleanArchitecture/PublicHeader.h>
+
+
